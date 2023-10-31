d65420e08d9c2141a04740317eeda730fb1e4c02
Chibuike <chibuike.dmn@gmail.com>
2023-10-31 15:21:41 +0000

    feat: Anadido boton para modificar el tema

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import MortyInfo from './component/MortyInfo';
 import RickInfo from './component/RickInfo';
 import QRCodeComponent from './component/QRCode';
@@ -8,54 +8,48 @@ export default function App() {
   const [displayMyQR, setDisplayMyQR] = useState(false);
   const [displayMortyInfo, setDisplayMortyInfo] = useState(false);
   const [displayRickInfo, setDisplayRickInfo] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
 
-  const handleShowQR = () => {
-    setDisplayMyQR(true);
-    setDisplayMortyInfo(false);
-    setDisplayRickInfo(false);
-  };
-
-  const handleShowMortyInfo = () => {
-    setDisplayMyQR(false);
-    setDisplayMortyInfo(true);
-    setDisplayRickInfo(false);
-  };
-
-  const handleShowRickInfo = () => {
-    setDisplayMyQR(false);
-    setDisplayMortyInfo(false);
-    setDisplayRickInfo(true);
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
   };
 
   return (
-    <ScrollView style={styles.container}>
+    <ScrollView style={isDarkTheme ? styles.containerDark : styles.containerLight}>
       <View style={styles.menuContainer}>
-        <TouchableOpacity onPress={handleShowMortyInfo} style={styles.menuOption}>
-          <Text style={styles.menuText}>Morty Smith</Text>
+        <TouchableOpacity onPress={() => {setDisplayMyQR(false);setDisplayMortyInfo(true);setDisplayRickInfo(false);}} style={styles.menuOption}>
+          <Text style={styles.menuText}>MORTY</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={handleShowRickInfo} style={styles.menuOption}>
-          <Text style={styles.menuText}>Rick Sanchez</Text>
+        <TouchableOpacity onPress={() => {setDisplayMyQR(false);setDisplayMortyInfo(false);setDisplayRickInfo(true);}} style={styles.menuOption}>
+          <Text style={styles.menuText}>RICK</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={handleShowQR} style={styles.menuOption}>
-          <Text style={styles.menuText}>Mi Repositorio</Text>
+        <TouchableOpacity onPress={() => {setDisplayMyQR(true);setDisplayMortyInfo(false);setDisplayRickInfo(false);}} style={styles.menuOption}>
+          <Text style={styles.menuText}>MiINFO</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={toggleTheme} style={{ ...styles.menuOption, borderWidth: 1, borderColor: isDarkTheme ? '#66D1A3' : 'white' }}>
+          <Text style={styles.menuText2}>TEMA</Text>
         </TouchableOpacity>
       </View>
       {displayMyQR ? <QRCodeComponent /> : null}
-      {displayMortyInfo ? <MortyInfo /> : null}
-      {displayRickInfo ? <RickInfo /> : null}
+      {displayMortyInfo ? <MortyInfo isDarkTheme={isDarkTheme} /> : null}
+      {displayRickInfo ? <RickInfo isDarkTheme={isDarkTheme} /> : null}
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerDark: {
     flex: 1,
     backgroundColor: '#191919',
+  },
+  containerLight: {
+    flex: 1,
+    backgroundColor: 'white',
   },
   menuContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
-    backgroundColor: '#333333', 
+    backgroundColor: '#333333',
     padding: 20,
   },
   menuOption: {
@@ -64,8 +58,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   menuText: {
-    color: '#66D1A3', 
-    fontSize: 18,
+    color: '#66D1A3',
+    fontSize: 13,
     fontWeight: 'bold',
   },
+  menuText2: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: 'bold',
+  }
 });
